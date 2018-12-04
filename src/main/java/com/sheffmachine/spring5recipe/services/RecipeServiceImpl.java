f1f547e2c2b4ada4f8319e4498e9972f48497006
SHEFFmachine <sheff@sheffmachine.com>
2018-12-03 20:45:14 -0800

    Add bootstrap for recipes

---
+++ src/main/java/com/sheffmachine/spring5recipe/services/RecipeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sheffmachine.spring5recipe.services;
+
+import com.sheffmachine.spring5recipe.domain.Recipe;
+import com.sheffmachine.spring5recipe.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
