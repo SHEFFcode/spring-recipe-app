e429050e12c19b36c887ffc2e2addd64c9cc8a84
SHEFFmachine <sheff@sheffmachine.com>
2019-01-12 20:21:49 -0800

    Add transactional annotations

--- src/main/java/com/sheffmachine/spring5recipe/Bootstrap.java
+++ src/main/java/com/sheffmachine/spring5recipe/Bootstrap.java
@@ -10,6 +10,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import javax.swing.text.html.Option;
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +30,7 @@ public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
         log.debug("Loading bootstrap data...");
